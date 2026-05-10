af7598336dbce09b133bc22c0ab04ad778b78034
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2026-05-10 00:48:14 -0700

    add `doom`

---
+++ tests/riscv-toolchain.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR riscv64)
+
+set(CMAKE_C_COMPILER riscv-none-elf-gcc)
+set(CMAKE_CXX_COMPILER riscv-none-elf-g++)
+set(CMAKE_ASM_COMPILER riscv-none-elf-gcc)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_EXECUTABLE_SUFFIX ".elf")
